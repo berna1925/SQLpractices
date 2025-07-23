88c98e6cc2f9f490658c55c0445b7d55f4e30461
berna1925 <forscher2121@gmail.com>
2025-07-23 18:17:16 +0900

    숫자 앤드 조건은 숫자끼리에 괄호를 넣자

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,3 +1,16 @@
+# 2025-07-23 18:10
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS AS D
+WHERE (SKILL_CODE & (SELECT S.CODE FROM SKILLCODES AS S WHERE NAME = 'Python') != 0)
+      OR (SKILL_CODE & (SELECT S.CODE FROM SKILLCODES AS S WHERE NAME = 'C#') != 0)
+ORDER BY ID ;
+
+# 공통점이 없는 두 테이블에 JOIN이 필요 없다는 것을 알고 다르게 접근한 것이 좋았음
+# 다만 WHERE 절이 지나치게 지저분하며 숫자끼리의 & 조건만 우선 괄호로 감싸주는 것을 놓쳤음
+# WITH 절로 미리 잡다한 코드를 정리하고 들어가는 것이 좋다는 권고
+
+===========================================================================================
+
 # SELECT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
 # FROM DEVELOPERS D
 # WHERE (D.SKILL_CODE & 256) > 0 
@@ -11,11 +24,6 @@
 # ORDER BY d.ID;
 
 
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPERS AS D
-WHERE (SKILL_CODE & (SELECT S.CODE FROM SkILLCODES AS S WHERE NAME = 'Python') != 0)
-      OR (SKILL_CODE & (SELECT S.CODE FROM SkILLCODES AS S WHERE NAME = 'C#') != 0)
-ORDER BY ID ;
 
 
 
