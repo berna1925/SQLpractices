0833c01d03b475ab37fb3f4b5fb21d7dee95d143
berna1925 <forscher2121@gmail.com>
2025-07-24 16:52:46 +0900

    연산자 같은 기본을 유연하게 활용하자

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,4 +1,5 @@
--- 코드를 작성해주세요
+# 2025-07-24 못 푼 문제
+# 예전부터 약점이었던 계층 구조 문제에서 발목
 SELECT ITEM_ID, ITEM_NAME, RARITY
 FROM ITEM_INFO 
 WHERE ITEM_ID IN (SELECT T.ITEM_ID
@@ -6,6 +7,11 @@ WHERE ITEM_ID IN (SELECT T.ITEM_ID
                  JOIN ITEM_INFO I
                  ON T.PARENT_ITEM_ID = I.ITEM_ID
                  WHERE I.RARITY = 'RARE')
+
+# 서브쿼리를 쓰는 문제는 항상 단일한 행이 나오도록 출력 값을 정해줘야 함
+# 그러나 비교 연산자가 =가 아니라 IN이라면 리스트 안에 있는 값 중 조건을 만족하는 모든 값을 선택 가능
+# 서브쿼리 오류를 보고 아예 서브쿼리를 쓸 생각을 못한 것이 문제
+  
 ORDER BY ITEM_ID DESC;
 
 
