a47b6c03748734fff253b0035298582684cf8fc5
berna1925 <forscher2121@gmail.com>
2025-07-29 18:38:16 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,17 +1,15 @@
 -- 코드를 입력하세요
-WITH BINDING AS (
-    SELECT ANIMAL_ID, DATE_FORMAT(DATETIME, '%H') AS HOUR
-    FROM ANIMAL_OUTS
+# WITH BINDING AS (
+#     SELECT ANIMAL_ID, DATE_FORMAT(DATETIME, '%H') AS HOUR
+#     FROM ANIMAL_OUTS
+# )
 
-SELECT HOUR, COUNT(ANIMAL_ID) AS COUNT
-FROM BINDING
-GROUP BY HOUR
-ORDER BY 1 ;
-# 이 코드는 입양 시간별로 입양 나간 마릿수를 구할 수는 있으나 문제에서 요구하는 건 입양이 없었던 시간대도 집계하는 것
-# date_format은 이미 있는 자료의 형태를 변환하는 것이므로 함수적으로 적용해서는 답을 반환할 수 없음
+# SELECT HOUR, COUNT(ANIMAL_ID) AS COUNT
+# FROM BINDING
+# GROUP BY HOUR
+# # ORDER BY 1 ;
 
-
-WITH hours AS (
+WITH HOURS AS (
     SELECT 0 AS HOUR UNION ALL
     SELECT 1 UNION ALL
     SELECT 2 UNION ALL
@@ -37,14 +35,26 @@ WITH hours AS (
     SELECT 22 UNION ALL
     SELECT 23
 )
-# WITH문으로 가상의 데이터를 먼저 만든 다음에 붙인다는 발상은 좋았음
-# 그런데 없는 데이터를 어떻게 만들지를 생각해내지 못한 것이 문제
-# JOIN은 물론이고 UNION도 생각보다 활용 방법이 다양하다. 이것을 어떻게 활용할지 생각하는 것도 유익할 듯하다.
-# 파이썬에 CONCAT이 있다면 SQL엔 UNION ALL이 있다. 수직으로 붙일 때는 바로 UNION을 생각하자.
-    
-SELECT h.HOUR, COUNT(ao.ANIMAL_ID) AS COUNT
-FROM hours h
-LEFT JOIN ANIMAL_OUTS ao
-    ON HOUR(ao.DATETIME) = h.HOUR
-GROUP BY h.HOUR
-ORDER BY h.HOUR;
+
+SELECT H.HOUR, COUNT(A.ANIMAL_ID) AS COUNT
+FROM HOURS H
+LEFT JOIN ANIMAL_OUTS A
+    ON HOUR(A.DATETIME) = H.HOUR
+GROUP BY H.HOUR
+ORDER BY H.HOUR ;
+
+
+
+
+
+# WITH CT_TABLE AS (
+# SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) AS CT
+# FROM ANIMAL_OUTS
+# GROUP BY HOUR(DATETIME)
+# ORDER BY HOUR
+# )
+
+# SELECT 
+
+
+
