7639fb48708cd02bbd089ece9b698bc39a9deac1
berna1925 <forscher2121@gmail.com>
2025-07-31 15:40:18 +0900

    CASE WHEN의 병렬 연결 불가 원리

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,26 +1,8 @@
-# WITH GRADING AS (
-#     SELECT E.EMP_NO, E.EMP_NAME, SAL,
-#             CASE WHEN AVG(SCORE) >= 96 THEN 'S'
-#                  WHEN AVG(SCORE) >= 90 THEN 'A'
-#                  WHEN AVG(SCORE) >= 80 THEN 'B'
-#             ELSE 'C' END AS GRADE
-#     FROM HR_EMPLOYEES E JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
-#     GROUP BY E.EMP_NO
-#     )
-
-# SELECT G.EMP_NO, G.EMP_NAME, GRADE,
-#        CASE WHEN GRADE = 'S' THEN SAL * 0.2
-#             WHEN GRADE = 'A' THEN SAL * 0.15
-#             WHEN GRADE = 'B' THEN SAL * 0.1
-#        ELSE 0 END AS BONUS
-# FROM GRADING G
-# ORDER BY G.EMP_NO ;
-    
-    
-    
-    
+# 2025-07-31 15:30 자력 해결
 WITH GRADE_TABLE AS (
     SELECT EMP_NO,
+    # CASE WHEN문의 작성 결과를 바로 오른쪽 열에서 사용은 불가능
+    # 베이스가 될 열을 CASE WHEN으로 미리 연산해 WITH나 VIEW에 저장해 놓고 JOIN으로 연동해 메인쿼리에서 활용해야
         CASE WHEN AVG(SCORE) >= 96 THEN 'S'
              WHEN AVG(SCORE) >= 90 THEN 'A'
              WHEN AVG(SCORE) >= 80 THEN 'B'
@@ -42,3 +24,24 @@ JOIN GRADE_TABLE AS GT
 ON E.EMP_NO = GT.EMP_NO
 GROUP BY E.EMP_NO, E.EMP_NAME
 ORDER BY 1 ;
+
+
+
+# WITH GRADING AS (
+#     SELECT E.EMP_NO, E.EMP_NAME, SAL,
+#             CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+#                  WHEN AVG(SCORE) >= 90 THEN 'A'
+#                  WHEN AVG(SCORE) >= 80 THEN 'B'
+#             ELSE 'C' END AS GRADE
+#     FROM HR_EMPLOYEES E JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
+#     GROUP BY E.EMP_NO
+#     )
+
+# SELECT G.EMP_NO, G.EMP_NAME, GRADE,
+#        CASE WHEN GRADE = 'S' THEN SAL * 0.2
+#             WHEN GRADE = 'A' THEN SAL * 0.15
+#             WHEN GRADE = 'B' THEN SAL * 0.1
+#        ELSE 0 END AS BONUS
+# FROM GRADING G
+# ORDER BY G.EMP_NO ;
+
