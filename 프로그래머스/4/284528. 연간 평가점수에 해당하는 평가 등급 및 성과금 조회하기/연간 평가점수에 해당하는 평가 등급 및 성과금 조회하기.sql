dd339cee73ccd8c9c207c49a72131c7379cce717
berna1925 <forscher2121@gmail.com>
2025-07-31 15:31:53 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,17 +1,44 @@
-WITH GRADING AS (
-    SELECT E.EMP_NO, E.EMP_NAME, SAL,
-            CASE WHEN AVG(SCORE) >= 96 THEN 'S'
-                 WHEN AVG(SCORE) >= 90 THEN 'A'
-                 WHEN AVG(SCORE) >= 80 THEN 'B'
-            ELSE 'C' END AS GRADE
-    FROM HR_EMPLOYEES E JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
-    GROUP BY E.EMP_NO
-    )
+# WITH GRADING AS (
+#     SELECT E.EMP_NO, E.EMP_NAME, SAL,
+#             CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+#                  WHEN AVG(SCORE) >= 90 THEN 'A'
+#                  WHEN AVG(SCORE) >= 80 THEN 'B'
+#             ELSE 'C' END AS GRADE
+#     FROM HR_EMPLOYEES E JOIN HR_GRADE G ON E.EMP_NO = G.EMP_NO
+#     GROUP BY E.EMP_NO
+#     )
 
-SELECT G.EMP_NO, G.EMP_NAME, GRADE,
-       CASE WHEN GRADE = 'S' THEN SAL * 0.2
-            WHEN GRADE = 'A' THEN SAL * 0.15
-            WHEN GRADE = 'B' THEN SAL * 0.1
-       ELSE 0 END AS BONUS
-FROM GRADING G
-ORDER BY G.EMP_NO ;
+# SELECT G.EMP_NO, G.EMP_NAME, GRADE,
+#        CASE WHEN GRADE = 'S' THEN SAL * 0.2
+#             WHEN GRADE = 'A' THEN SAL * 0.15
+#             WHEN GRADE = 'B' THEN SAL * 0.1
+#        ELSE 0 END AS BONUS
+# FROM GRADING G
+# ORDER BY G.EMP_NO ;
+    
+    
+    
+    
+WITH GRADE_TABLE AS (
+    SELECT EMP_NO,
+        CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+             WHEN AVG(SCORE) >= 90 THEN 'A'
+             WHEN AVG(SCORE) >= 80 THEN 'B'
+         ELSE 'C' END AS GRADE
+    FROM HR_GRADE G
+    GROUP BY G.EMP_NO
+)    
+    
+SELECT
+    E.EMP_NO,
+    E.EMP_NAME,
+    GT.GRADE,
+    CASE WHEN GT.GRADE = 'S' THEN E.SAL * 0.2
+         WHEN GT.GRADE = 'A' THEN E.SAL * 0.15
+         WHEN GT.GRADE = 'B' THEN E.SAL * 0.1
+         ELSE 0 END AS BONUS
+FROM HR_EMPLOYEES AS E
+JOIN GRADE_TABLE AS GT
+ON E.EMP_NO = GT.EMP_NO
+GROUP BY E.EMP_NO, E.EMP_NAME
+ORDER BY 1 ;
